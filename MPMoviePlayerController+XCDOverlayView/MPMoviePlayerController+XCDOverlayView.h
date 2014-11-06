335fd9e2b88023d0a87c50762614a95a3f101159
Cédric Luthi <cedric.luthi@gmail.com>
2014-11-06 10:12:17 +0100

    Extract MPMoviePlayerController+XCDOverlayView from XCDYouTubeKit

---
+++ MPMoviePlayerController+XCDOverlayView/MPMoviePlayerController+XCDOverlayView.h
@@ -0,0 +1,11 @@
+//
+//  Copyright (c) 2014 Cédric Luthi. All rights reserved.
+//
+
+#import <MediaPlayer/MediaPlayer.h>
+
+@interface MPMoviePlayerController (XCDOverlayView)
+
+@property (nonatomic, strong) UIView *overlayView_xcd;
+
+@end
