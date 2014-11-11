ddc8911ce9038067a6981ccb46cf91177642737b
Cédric Luthi <cedric.luthi@gmail.com>
2014-11-11 23:41:40 +0100

    Replace the copyright label with a button

--- XCDOverlayView Demo/XCDOverlayView Demo/OverlayView.m
+++ XCDOverlayView Demo/XCDOverlayView Demo/OverlayView.m
@@ -7,7 +7,7 @@
 @interface OverlayView ()
 
 @property (nonatomic, weak) IBOutlet UILabel *titleLabel;
-@property (nonatomic, weak) IBOutlet UILabel *copyrightLabel;
+@property (nonatomic, weak) IBOutlet UIButton *copyrightButton;
 
 @end
 
@@ -17,7 +17,7 @@
 {
 	OverlayView *overlayView = [[[UINib nibWithNibName:@"OverlayView" bundle:nil] instantiateWithOwner:nil options:nil] firstObject];
 	overlayView.titleLabel.text = title;
-	overlayView.copyrightLabel.text = copyright;
+	[overlayView.copyrightButton setTitle:copyright forState:UIControlStateNormal];
 	return overlayView;
 }
 
@@ -27,6 +27,34 @@
 	[UIView animateWithDuration:0.3f animations:^{
 		self.alpha = 1.f;
 	}];
+}
+
+- (NSURL *) copyrightURL
+{
+	NSDataDetector *linkDataDetector = [NSDataDetector dataDetectorWithTypes:NSTextCheckingTypeLink error:NULL];
+	NSString *copyright = [self.copyrightButton titleForState:UIControlStateNormal];
+	NSTextCheckingResult *result = [linkDataDetector firstMatchInString:copyright options:(NSMatchingOptions)0 range:NSMakeRange(0, copyright.length)];
+	return result.URL;
+}
+
+- (IBAction) copyright:(id)sender
+{
+	NSURL *copyrightURL = self.copyrightURL;
+	NSString *message = copyrightURL ? [NSString stringWithFormat:@"Do you want to open %@ in Safari?", copyrightURL] : @"No URL was found in the copyright.";
+	NSString *cancelButtonTitle = copyrightURL ? @"Cancel" : @"OK";
+	NSString *otherButtonTitle = copyrightURL ? @"Open URL" : nil;
+	UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:nil message:message delegate:self cancelButtonTitle:cancelButtonTitle otherButtonTitles:otherButtonTitle, nil];
+	[alertView show];
+}
+
+#pragma mark - UIAlertViewDelegate
+
+- (void) alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+	if (buttonIndex == alertView.cancelButtonIndex)
+		return;
+	
+	[[UIApplication sharedApplication] openURL:self.copyrightURL];
 }
 
 @end
